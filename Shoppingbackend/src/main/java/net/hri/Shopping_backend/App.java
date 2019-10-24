68fae36954f2eb38e26d8614e86165309c3ef5b5
Hritvik3 <hritvikpatwa@gmail.com>
2019-10-24 10:58:16 +0530

    first step completed successfully

---
+++ Shoppingbackend/src/main/java/net/hri/Shopping_backend/App.java
@@ -0,0 +1,13 @@
+package net.hri.Shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ onlineshopping/src/main/java/net/hri/onlineshopping/controller/PageController.java
@@ -0,0 +1,27 @@
+package net.hri.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController{
+@RequestMapping(value= {"/", "/home", "/index"}, method=RequestMethod.GET)
+public ModelAndView index() {
+	ModelAndView mv= new ModelAndView("page");
+	mv.addObject("greeting", "Welcome to spring mvc!!");
+	return mv;
+	
+}
+
+@RequestMapping(value="/test")
+public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+	ModelAndView mv= new ModelAndView("page");
+	mv.addObject("greeting", greeting);
+	return mv;
+	
+}
+
+}
